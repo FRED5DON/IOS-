7f0e87be7ee47e1521e473b4c5878e6c0d59d757
MacBook On Dell <freddon_mac@MacdeMacBook-Pro.local>
2015-06-28 16:04:38 +0800

    Initial Commit

---
+++ IOS笔记/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  IOS笔记
+//
+//  Created by MacBook On Dell on 15-6-28.
+//  Copyright (c) 2015年 Fred.Don. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ IOS笔记/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  IOS笔记
+//
+//  Created by MacBook On Dell on 15-6-28.
+//  Copyright (c) 2015年 Fred.Don. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
